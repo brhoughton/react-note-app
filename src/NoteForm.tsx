dd885337da02b9fd92fcf4334c493a24e83c7394
Breck Houghton <breck.houghton@gmail.com>
2024-01-02 11:10:04 -0800

    Optimized files and started building components

---
+++ src/NoteForm.tsx
@@ -0,0 +1,14 @@
+import { Form, Stack } from "react-bootstrap";
+
+export function NoteForm() {
+  return (
+    <Form>
+      <Stack gap={4}>
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+        <div>4</div>
+      </Stack>
+    </Form>
+  );
+}
